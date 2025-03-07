276c6d7fbd892607a5c5d17e4bf2e0b720eee8be
Stanislav <boar_100@mail.ru>
2025-03-07 17:20:54 +0500

    Initial Commit

---
+++ Instant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instant
+//
+//  Created by Станислав Витальевич on 07.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
