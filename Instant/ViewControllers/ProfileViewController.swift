1f7aa7833af5ace76b650f17661ee519c494b70b
Stanislav <boar_100@mail.ru>
2025-03-08 17:36:07 +0500

    Improved profileviewcontroller user interface

--- Instant/ViewControllers/ProfileViewController.swift
+++ Instant/ViewControllers/ProfileViewController.swift
@@ -6,14 +6,25 @@ class ProfileViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var avatarImageView: UIImageView!
-    
     @IBOutlet weak var usernameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        avatarImageView.image = UIImage(systemName: "person.fill")
+        let avatarTap = UITapGestureRecognizer(target: self, action: #selector(presentAvatarOptions))
+        avatarImageView.isUserInteractionEnabled = true
+        avatarImageView.addGestureRecognizer(avatarTap)
     }
     
+    // здесь лучше всего делать закругление углов
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        containerView.layer.cornerRadius = 8
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.width / 2
+        avatarImageView.tintColor = UIColor.black
+        avatarImageView.backgroundColor = UIColor.lightGray
+        
+    }
     // полный выход из аккаунта, после выхода пользователя из аккаунта направляем его обратно на страницу входа в аккаунт
     func logout() {
         do {
@@ -27,6 +38,26 @@ class ProfileViewController: UIViewController {
         }
     }
     
+    @objc func presentAvatarOptions() {
+        let avatarOptionsSheet = UIAlertController(title: "Change Avatar", message: "Select an option.", preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title:"Camera", style: .default) { _ in
+            
+        }
+        let photoAction = UIAlertAction(title: "Photo", style: .default) { _ in
+            
+        }
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel ", style: .cancel)
+        
+        avatarOptionsSheet.addAction(cameraAction)
+        avatarOptionsSheet.addAction(photoAction)
+        avatarOptionsSheet.addAction(deleteAction)
+        avatarOptionsSheet.addAction(cancelAction)
+        present(avatarOptionsSheet, animated: true)
+    }
+    
     @IBAction func logoutButtonTapped(_ sender: PrimaryButton) {
         let logoutAlert = UIAlertController(title: "Confirm Logout", message: "Are you sure you would like to logout of your account?", preferredStyle: .alert)
         let logoutAction = UIAlertAction(title: "Logout", style: .destructive) { _ in
@@ -38,4 +69,7 @@ class ProfileViewController: UIViewController {
         present(logoutAlert, animated: true)
     }
     
+    @IBAction func dismissButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
 }
